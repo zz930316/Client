8fd91f9a63f7cb0357dacd23b3cf88d8bc49476a
zz930316 <33750354+zz930316@users.noreply.github.com>
2017-11-24 15:49:21 +0800

    Update 源.cpp

--- Client/源.cpp
+++ Client/源.cpp
@@ -8,56 +8,57 @@ using namespace std;
 void main()
 {
 	WSADATA wsaData;
-	//Ҫ�������ӿ⺯��������Ϊ�������ϵͳ˵��������Ҫ���ĸ����ļ����øÿ��ļ��뵱ǰ��Ӧ�ó���󶨣��Ӷ��Ϳ��Ե��øð汾��socket�ĸ��ֺ����ˡ�
-	//��winsock DLL����̬���ӿ��ļ������г�ʼ����Э��Winsock�İ汾֧�֣��������Ҫ����Դ��  
+	//要添加链接库函数，就是为了向操作系统说明，我们要用哪个库文件，让该库文件与当前的应用程序绑定，从而就可以调用该版本的socket的各种函数了。
+	//对winsock DLL（动态链接库文件）进行初始化，协商Winsock的版本支持，并分配必要的资源。  
+	//MAKEWORD(1, 1)和MAKEWORD(2, 2)的区别在于，前者只能一次接收一次，不能马上发送，而后者能
 	WSAStartup(MAKEWORD(1, 1), &wsaData);
 
-	/*����socket;
-	AF_INET��ʾ��Internet��ͨ�ţ�
-	SOCK_STREAM��ʾsocket�����׽��֣���Ӧtcp��
-	0ָ������Э��ΪTCP/IP */
+	/*创建socket;
+	AF_INET表示在Internet中通信；
+	SOCK_STREAM表示socket是流套接字，对应tcp；
+	0指定网络协议为TCP/IP */
 	SOCKET sockClient = socket(AF_INET, SOCK_STREAM, 0);
 
-	SOCKADDR_IN addrClt;//����һ���׽��ִ�ŵ�ַ�Ͷ˿�;
+	SOCKADDR_IN addrClt;//创建一个套接字存放地址和端口;
 
 
-						/*addrClt����׽����Ǵ��Ŀ�꣨����������IP��ַ�Ͷ˿ڵ�*/
-						//addr����ַ    port���˿�
-	addrClt.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");// inet_addr��IP��ַ�ӵ�����ʽת���������ֽڸ�ʽ���͡�
-	addrClt.sin_family = AF_INET;//AF_INET�ǻ�������:UDP TCP�ȡ�
-	addrClt.sin_port = htons(4000);//htons�����������ֽ�˳��ת��Ϊ�����ֽ�˳��(to network short) 
+						/*addrClt这个套接字是存放目标（服务器）的IP地址和端口的*/
+						//addr：地址    port：端口
+	addrClt.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");// inet_addr将IP地址从点数格式转换成网络字节格式整型。
+	addrClt.sin_family = AF_INET;//AF_INET是互联网络:UDP TCP等。
+	addrClt.sin_port = htons(4000);//htons用来将主机字节顺序转换为网络字节顺序(to network short) 
 
-								   //�ͻ����������������������  �ú����Ĺ���Ϊ�ͻ����������ӷ�����;
+								   //客户机向服务器发出连接请求  该函数的功能为客户端主动连接服务器;
 
 	connect(sockClient, (SOCKADDR*)&addrClt, sizeof(SOCKADDR));
 	while (true)
 	{
 		char recvBuf[50];
-		/*���ն˷�����������:
-		sockfd  :  ���ն��׽���������;
-		buff    :  �������recv�������յ������ݵĻ�����;
-		nbytes  :  ָ��buff�ĳ���;
-		flags   :  һ����Ϊ0;
+		/*接收端发送来的数据:
+		sockfd  :  接收端套接字描述符;
+		buff    :  用来存放recv函数接收到的数据的缓冲区;
+		nbytes  :  指明buff的长度;
+		flags   :  一般置为0;
 		recv(int sockfd, void *buff, size_t nbytes, int flags);*/
 		recv(sockClient, recvBuf, 50, 0);
 
-		cout << "����˻�Ӧ : " << recvBuf << endl; //printf("����˻�Ӧ : %s\n", recvBuf);
+		cout << "服务端回应 : " << recvBuf << endl; //printf("服务端回应 : %s\n", recvBuf);
 
 		char sendBuf[50];
 		//sprintf(sendBuf, "%3d,", index);
-		strcpy(sendBuf, "�ͻ��˷����������ݣ�hello world!");
-		/*�������ݶ�:
-		sockfd :  ָ�����Ͷ��׽���������;
-		buff   :  ���Ҫ�������ݵĻ�����;
-		nbytes :  ʵ��Ҫ���Ƶ����ݵ��ֽ���;
-		flags  :  һ����Ϊ0;
+		strcpy(sendBuf, "客户端发送来的数据：hello world!");
+		/*发送数据端:
+		sockfd :  指定发送端套接字描述符;
+		buff   :  存放要发送数据的缓冲区;
+		nbytes :  实际要改善的数据的字节数;
+		flags  :  一般置为0;
 		send(int sockfd, const void *buff, size_t nbytes, int flags);*/
 		send(sockClient, sendBuf, strlen(sendBuf) + 1, 0);
 
 		Sleep(2000);
 	}
-	closesocket(sockClient);//�ر�����;
+	closesocket(sockClient);//关闭连接;
 	WSACleanup();
-	/*WSAStartup�Ĺ����ǳ�ʼ��Winsock DLL��
-	WSACleanup���������Socket��İ󶨲����ͷ�Socket����ռ�õ�ϵͳ��Դ��*/
+	/*WSAStartup的功能是初始化Winsock DLL，
+	WSACleanup是来解除与Socket库的绑定并且释放Socket库所占用的系统资源。*/
 }
